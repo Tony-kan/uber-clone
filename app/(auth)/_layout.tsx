e1ce73fe65f863d77705659e1d0cd00c1cf11a16
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-12-13 03:34:02 +0200

    setup : added assets,created file & folder structure,created types  for images and  types

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+// import "../../global.css";
+
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+    </Stack>
+    // <StatusBar style="auto" />
+  );
+}
---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+// import "../../../global.css";
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+      <Stack.Screen name="chat" options={{ headerShown: false }} />
+      <Stack.Screen name="profile" options={{ headerShown: false }} />
+      <Stack.Screen name="rides" options={{ headerShown: false }} />
+    </Stack>
+    // <StatusBar style="auto" />
+  );
+}
