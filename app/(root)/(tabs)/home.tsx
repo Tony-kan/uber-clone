6a25ef084689e421c4ebc8e576474b5a97a1eff5
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-12-17 22:30:01 +0200

    Added MockData:for drivers and rides to assist in building ui.Added lib:map for accessing google maps,utils-for formating dates sorting drivers & other.Scripts:sql scripts to seed drivers  table with data and get drivers data from db

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,11 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SignedIn, SignedOut, useUser, useClerk } from "@clerk/clerk-expo";
 
 import { Link } from "expo-router";
 import * as Linking from "expo-linking";
 
+const recentRides = [];
 const Home = () => {
   const { user } = useUser();
   const { signOut } = useClerk();
@@ -23,23 +24,27 @@ const Home = () => {
 
   return (
     <SafeAreaView>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <TouchableOpacity
-          className="bg-primary-500 border border-primary-500 w-16 h-16"
-          onPress={handleSignOut}
-        >
-          <Text>Sign Out</Text>
-        </TouchableOpacity>
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text className="text-black">Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text className="text-black">Sign up</Text>
-        </Link>
-      </SignedOut>
+      <FlatList
+        data={recentRides?.slice(0, 5)}
+        renderItem={({ item }) => <RideCard ride={item} />}
+      />
+      {/*<SignedIn>*/}
+      {/*  <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>*/}
+      {/*  <TouchableOpacity*/}
+      {/*    className="bg-primary-500 border border-primary-500 w-16 h-16"*/}
+      {/*    onPress={handleSignOut}*/}
+      {/*  >*/}
+      {/*    <Text>Sign Out</Text>*/}
+      {/*  </TouchableOpacity>*/}
+      {/*</SignedIn>*/}
+      {/*<SignedOut>*/}
+      {/*  <Link href="/(auth)/sign-in">*/}
+      {/*    <Text className="text-black">Sign in</Text>*/}
+      {/*  </Link>*/}
+      {/*  <Link href="/(auth)/sign-up">*/}
+      {/*    <Text className="text-black">Sign up</Text>*/}
+      {/*  </Link>*/}
+      {/*</SignedOut>*/}
     </SafeAreaView>
   );
 };
---
+++ scripts/seed_drivers.sql
@@ -0,0 +1,6 @@
+INSERT INTO drivers (id, first_name, last_name, profile_image_url, car_image_url, car_seats, rating)
+VALUES
+('1', 'James', 'Wilson', 'https://ucarecdn.com/dae59f69-2c1f-48c3-a883-017bcf0f9950/-/preview/1000x666/', 'https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/', 4, '4.80'),
+('2', 'David', 'Brown', 'https://ucarecdn.com/6ea6d83d-ef1a-483f-9106-837a3a5b3f67/-/preview/1000x666/', 'https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/', 5, '4.60'),
+('3', 'Michael', 'Johnson', 'https://ucarecdn.com/0330d85c-232e-4c30-bd04-e5e4d0e3d688/-/preview/826x822/', 'https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/', 4, '4.70'),
+('4', 'Robert', 'Green', 'https://ucarecdn.com/fdfc54df-9d24-40f7-b7d3-6f391561c0db/-/preview/626x417/', 'https://ucarecdn.com/b6fb3b55-7676-4ff3-8484-fb115e268d32/-/preview/930x932/', 4, '4.90');
