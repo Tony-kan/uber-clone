3f897235341b9bbc2e1ae617ede839309eb7f58b
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-12-18 00:36:55 +0200

    Home : created ridecard,google TextInput

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,14 +1,27 @@
-import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  Image,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SignedIn, SignedOut, useUser, useClerk } from "@clerk/clerk-expo";
-
+import mock_rides from "../../../MockData/mock_rides.json";
 import { Link } from "expo-router";
 import * as Linking from "expo-linking";
+import RideCard from "@/components/RideCard";
+import { icons, images } from "@/constants";
+import React from "react";
+import GoogleTextInput from "@/components/GoogleTextInput";
 
-const recentRides = [];
+const recentRides = mock_rides;
+
 const Home = () => {
   const { user } = useUser();
   const { signOut } = useClerk();
+  const loading = false;
 
   const handleSignOut = async () => {
     try {
@@ -22,29 +35,68 @@ const Home = () => {
     }
   };
 
+  const handleDestinationPress = () => {};
+
   return (
     <SafeAreaView>
       <FlatList
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40"
+                  alt="no recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No recent rides</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#000" />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-xl text-2xl font-JakartaExtraBold capitalize">
+                Welcome {" , "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}
+              </Text>
+              <TouchableOpacity
+                className="justify-center  items-center w-10 h-10 rounded-full bg-white"
+                onPress={handleSignOut}
+              >
+                <Image source={icons.out} className="w-4 h-4" />
+              </TouchableOpacity>
+            </View>
+
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+
+            <>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Your Current Location
+              </Text>
+              <View className="flex flex-row items-center bg-transparent h-[300px]">
+                {/*map compent*/}
+              </View>
+            </>
+          </>
+        )}
       />
-      {/*<SignedIn>*/}
-      {/*  <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>*/}
-      {/*  <TouchableOpacity*/}
-      {/*    className="bg-primary-500 border border-primary-500 w-16 h-16"*/}
-      {/*    onPress={handleSignOut}*/}
-      {/*  >*/}
-      {/*    <Text>Sign Out</Text>*/}
-      {/*  </TouchableOpacity>*/}
-      {/*</SignedIn>*/}
-      {/*<SignedOut>*/}
-      {/*  <Link href="/(auth)/sign-in">*/}
-      {/*    <Text className="text-black">Sign in</Text>*/}
-      {/*  </Link>*/}
-      {/*  <Link href="/(auth)/sign-up">*/}
-      {/*    <Text className="text-black">Sign up</Text>*/}
-      {/*  </Link>*/}
-      {/*</SignedOut>*/}
     </SafeAreaView>
   );
 };
